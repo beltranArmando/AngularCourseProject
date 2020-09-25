154d505acccc3ef3ff7b19b028822d14e1113a2e
beltranArmando <armaabg@gmail.com>
2020-09-24 19:12:13 -0600

    add Services
    - managing recipes and shopping list in their own services.
    - using services for cross-component communication.
    - using services for pushing Data form A to B.
    -add Ingredients to Recipes.
    - passing ingredients form recipes to the shopping list (via a service).

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,19 +8,17 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('A test recipe', 'this is a test', '../../assets/images/Recipe_logo.jpeg'),
-    new Recipe('A second test recipe', 'this is a second test', '../../assets/images/Recipe_logo.jpeg')
-  ];
+  // @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
-  }
+  // onRecipeSelected(recipe: Recipe) {
+  //   this.recipeWasSelected.emit(recipe);
+  // }
 
 }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,38 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Recipe } from './recipe.model';
+
+@Injectable({ providedIn: 'root' })
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome!',
+      '../assets/images/TastySchnitzel.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe(
+      'Big Fat Burger',
+      'What else you need to say?',
+      '../assets/images/BigFatBurger.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ])
+  ];
+
+  constructor(private slService: ShoppingListService) { }
+
+  getRecipes() {
+    //use slice() to return a copy of the array and not the reference.
+    return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingLIst(ingredients: Ingredient[]) { 
+    this.slService.addIngredients(ingredients);
+  }
+}
