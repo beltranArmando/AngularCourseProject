f4cce1831a4a5c3c998b695dfea8878f7970bd61
beltranArmando <armaabg@gmail.com>
2020-10-09 18:33:00 -0600

    forms part 1
    - Add template driven form
    - Add validations to the form
    - Allowing the Selection of Items in the List
    - Loading the Shopping List Items into the Form
    - Updating existing Items
    - Resetting the Form
    - Allowing the user to clear the form
    - Allowing the Deletion of shopping list items
    - Create the template for the reactive recipe edit form
    - Create the form for editing recipes
    - synchronize HTML with the form
    - Adding ingredient controls to a form array

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -5,6 +5,7 @@ import { Ingredient } from '../shared/ingredient.model';
 @Injectable({ providedIn: 'root' })
 export class ShoppingListService {
   ingredientsChange = new Subject<Ingredient[]>();
+  startedEditing = new Subject<number>();
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10)
@@ -15,6 +16,10 @@ export class ShoppingListService {
     return this.ingredients.slice();
   }
 
+  getIngredient(index: number) {
+    return this.ingredients[index];
+  }
+
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
     this.ingredientsChange.next(this.ingredients.slice());
@@ -22,6 +27,16 @@ export class ShoppingListService {
 
   addIngredients(ingredients: Ingredient[]) {
     this.ingredients.push(...ingredients);
+    this.ingredientsChange.next(this.ingredients.slice());
+  }
+
+  updateIngredient(index: number, newIngredient: Ingredient) {
+    this.ingredients[index] = newIngredient;
+    this.ingredientsChange.next(this.ingredients.slice());
+  }
+
+  deleteIngredient(index: number) {
+    this.ingredients.splice(index, 1);
     this.ingredientsChange.next(this.ingredients.slice());
   }
 }
